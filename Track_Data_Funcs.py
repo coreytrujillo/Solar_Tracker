f35ecc14a7066434436b5a01b316eff40803c3b9
coreytrujillo <corey.trujill@colorado.edu>
2023-03-13 10:57:28 -0700

    Update Track_Data_Funcs library

--- Track_Data_Funcs.py
+++ Track_Data_Funcs.py
@@ -1,35 +1,32 @@
 # Import libraries
-from labjack import ljm # LabJack
+from labjack import ljm # LabJack functions
 from datetime import datetime # Date and time
 import numpy as np # Numbers and math
 import time # For sleep function, pef_counter
 import os # For removing files
 import sys # Scripting functions
-import ctypes
-import importlib
 import pandas as pd # For data analysis
 import ephem as ep # For predicting Sun's position
-# import Header
-# from Header import * # Global variables
 
 # CSV separator
 sep = ','
 
-# Connect with LabJack
+# Connect with LabJack and get handle and information
 def find_LJ():
 	# Look for LabJack
-	lookforLJ = True  
+	lookforLJ = True
+	print('Looking for LabJack...')
 	while lookforLJ:
 		# Try to find LabJack
 		try:
-			handle = ljm.openS('T7', 'ANY', 'ANY')  # T7 device, Any connection, Any identifier
+			handle = ljm.openS('ANY', 'ANY', 'ANY')  # Get handle (or ID) for T7 device, Any connection, Any identifier
 			info = ljm.getHandleInfo(handle) # LabJack Info
 			print('Connected to LabJack')
 			return(handle, info)
 			ljm.closeAll
 			lookforLJ = False
 			
-		# If LabJack isn't found
+		# If LabJack isn't found, it's likely not connected. This exception allows the user to connect it, then continue the code
 		except:
 			print('Not finding a LabJack Connection... \n')
 			while True:
@@ -69,7 +66,9 @@ def init_files(fd_path, fl_path, info, header):
 			fd.close()
 			break
 		
-		# If file can't initiate
+		# This exception adresses any issues with initiating a file
+		# This would usually be triggered when a file by the same name already exists
+		# It gives the user the option to exit the program, delete the existing file, or append it
 		except Exception as e:
 			print('File initiation failed:', e)
 			while needtooverwrite == True:
@@ -112,53 +111,104 @@ def init_files(fd_path, fl_path, info, header):
 					print('Invalid response. Please enter y for yes or n for no')
 
 # Append to data file
+# This is used when collecting data
 def append_data_file(fpath, outstr):
+	# fpath = path to file that's being appended
+	# outstr = string to append to file 
+
 	f = open(fpath, 'a') # Open file
 	f.write(outstr) # Append outstring to file
 	f.close() # Close file
 
 # Read Thermocouples from LabJack
-def read_LJ_TC(handle, TC_num, TC_AIN_start):
+def read_LJ_TC(handle, TC_num, TC_AIN_start, TC_AIN_vec, CJCON, CJCAIN):
+	# handle = LabJack T7 ID, should be acquired from find_LJ() function
+	# TC_num = number of thermocouples
+	# TC_AIN_start = first AIN TC's are plugged into ???????
+	# TC_AIN_vec = list of TC AINS. Set to 0 for only 1 TC ???????? 
+	# CJCON = Cold junciton compensation sensor on/off
+	# CJCAIN = AIN cold junction sensor is plugged into.
+	
 	# Initiate variables
 	ain_n = [] # Initiate vector for AIN's connected to TC's
 	ain_idx = [] # Initiate vector for TC type
-	ain_cnfg = [] # Initiate vector for Temperature scale
+	ain_cnfgA = [] # Initiate vector for Temperature scale
+	ain_cnfgB = [] # CJC AIN configuration
+	ain_cnfgD = [] # CJC slope
+	ain_cnfgE = [] # CJC offset
 	ain_rd =[] # Initiate vector of variable to read temps
 	
+	
 	# Create variables for thermocuple settings and measurement
-	for i in range(TC_AIN_start,TC_AIN_start+TC_num):
-		ain_n.append('AIN' + str(i)) # AIN numbers on Labjack
-		ain_idx.append(ain_n[i] + '_EF_INDEX') # Thermocouple type: 22 for K
-		ain_cnfg.append(ain_n[i] + '_EF_CONFIG_A') # Temperature scale configuration: 0 for K, 1 for C, 2 for F
-		ain_rd.append(ain_n[i] + '_EF_READ_A') # Reader variable
+	for i in range(0, TC_num):
+		
+		# If there's only one TC
+		if TC_AIN_vec == 0:
+			ain_n.append('AIN' + str(i + TC_AIN_start)) # Create name for AIN as input argument for ljm.eWriteNames
+		
+		# If there's more than on TC
+		else:
+			ain_n.append('AIN' + str(TC_AIN_vec[i])) # Create vector of AIN names for ljm.eWriteNames
+
+		ain_idx.append(ain_n[i] + '_EF_INDEX') # Input tag for ljm.eWriteNames: thermocouple type
+		ain_cnfgA.append(ain_n[i] + '_EF_CONFIG_A') # Input for ljm.eWriteNames: Temperature scale configuration: 0 for K, 1 for C, 2 for F
+		ain_rd.append(ain_n[i] + '_EF_READ_A') # Input for ljm.eWriteNames: Read thermocouple variable
+		
+		# If using Cold Junction Sensor, prepare configuration setting variables ???? Move down lower to consolidate CJC functions??????
+		if CJCON == 1:
+			ain_cnfgB.append(ain_n[i] + '_EF_CONFIG_B') # CJC AIN configuration
+			ain_cnfgD.append(ain_n[i] + '_EF_CONFIG_D') # CJC slope
+			ain_cnfgE.append(ain_n[i] + '_EF_CONFIG_E') # CJC offset
 	
 	# Indentify TC settings for LabJack
-	v_idx = 22*np.ones((TC_num,1),float) # Identify type K thermocouple
-	v_cnfg = np.ones((TC_num,1),float) # Set thermocouple to read in Celcius
+	v_idx = 22*np.ones((TC_num,1),float) # Input for ljm.eWriteNames: 22 = Type K thermocouple
+	cnfgA = np.ones((TC_num,1),float) # Input for ljm.eWriteNames: Set thermocouple to read in Celcius
 	
 	# Comminicate settings to LabJack
-	ljm.eWriteNames(handle, TC_num, ain_idx, v_idx) # Tell LabJack which AINs are Type K Thermocouples
-	ljm.eWriteNames(handle, TC_num, ain_cnfg, v_cnfg) # Tell LabJack temperature scale
+	ljm.eWriteNames(handle, TC_num, ain_idx, v_idx) # Identify which AINs are being used as Type K Thermocouples
+	ljm.eWriteNames(handle, TC_num, ain_cnfgA, cnfgA) # Configure LabJack for temperature scale
+	
+	# Setup Cold Juction Compensation Sensor, if being used
+	if CJCON == 1:
+		# Set configuration values for LM34
+		# See https://labjack.com/support/datasheets/t-series/ain/extended-features/thermocouple
+		cnfgB = CJCAIN*np.ones((TC_num,1),float) # AIN which LM34 is plugged into
+		cnfgD = 55.56*np.ones((TC_num,1),float) # CJC Slope for LM34
+		cnfgE = 255.37*np.ones((TC_num,1),float) # CJC offset for LM34. This value should always be 
+		
+		# Write values to LabJack ????????????
+		ljm.eWriteNames(handle, TC_num, ain_cnfgB, cnfgB)
+		ljm.eWriteNames(handle, TC_num, ain_cnfgD, cnfgD)
+		ljm.eWriteNames(handle, TC_num, ain_cnfgE, cnfgE)
 	
 	# Read temperature values from LabJack
 	Temps = ljm.eReadNames(handle, TC_num, ain_rd)
+	TCVs = ljm.eReadNames(handle, TC_num, ain_n)
 	
 	# Output Temperatures
-	return Temps
+	return Temps, TCVs
+
+# Read temperature values from cold junction temperature sensor LM34
+# See https://labjack.com/support/datasheets/t-series/ain/extended-features/thermocouple
+def read_LJ_CJC(handle, AIN):
+	# Set up CJC Temperature Sensor
+	CJCT = ljm.eReadName(handle, 'AIN' + str(AIN) + '_EF_READ_C') # Read temperature from LM34
+	CJCV = ljm.eReadName(handle, 'AIN' + str(AIN) + '_EF_READ_B') # Read calculated voltage for LM34
+	
+	return CJCT, CJCV
 
 # Read Pyrheliometer Voltage from LabJack
 def read_LJ_Pyr(handle, AIN):
 	# handle = LabJack handle
-	# AIN is Pyrheliometer AIN
-	
-	s = 7.85e-6 # Radiometer sensitivity [V/W/m2]
+	# AIN = AIN the Pyrheliometer is plugged into
+	s = 7.83e-6 # Radiometer sensitivity [V/W/m2]
 	Pyr_O = ljm.eReadName(handle, AIN) # Pyrheliometer output in Volts
 	Pyr_E = Pyr_O/s # Calculated Solar irradiance [W/m2]
 	
 	# Output values
 	return Pyr_O, Pyr_E
 
-# Run a Lua Script (for compass and accelerometer readings)
+# Run a Lua Script (for compass and accelerometer readings) ????????? Delete?????????
 def run_lua(handle, lua_s_name):
 	# handle = LabJack handle
 	# lua_s_name = name of lua script to run
@@ -182,7 +232,7 @@ def run_lua(handle, lua_s_name):
 	ljm.eWriteName(handle, 'LUA_DEBUG_ENABLE_DEFAULT', 0)
 	ljm.eWriteName(handle, 'LUA_RUN', 1)
 
-# Normalize magnetometer readings to outupt between -1 and 1
+# Normalize magnetometer readings to outupt between -1 and 1 ??????????? delete ???????????
 def normalize_mag(lo, hi, inp):
 	# lo = lowest possible magnetometer output
 	# hi = highest possible magnetometer output
@@ -199,7 +249,7 @@ def normalize_mag(lo, hi, inp):
 	return outp
 
 
-# Read Compass and Acceleromter
+# Read Compass and Acceleromter ??????????? Delete ????????????????
 def read_LJ_CA(handle):
 	# time.sleep(0.1) # Pause between readings 
 	mX0 = ljm.eReadName(handle, 'USER_RAM0_F32') # dirty mag x
@@ -225,7 +275,7 @@ def read_LJ_CA(handle):
 	
 	return mX, mY, mZ, aX, aY, aZ
 
-# Average Compass and accelerometer values to reduce noise
+# Average Compass and accelerometer values to reduce noise ?????????? Delete ????????????????
 def ave_CA(handle):
 	# Number of data points to average
 	loop = 1	
@@ -261,7 +311,7 @@ def ave_CA(handle):
 	return avemX, avemY, avemZ, aveaX, aveaY, aveaZ
 
 
-# Calculate elevation angle
+# Calculate elevation angle ???????????? Delete????????????????
 def el_ang(up, face):
 	# up = up vector
 	# face = vector that defines plane of the face of the collectors
@@ -273,7 +323,7 @@ def el_ang(up, face):
 	
 	return el
 
-# Calculate azimuthal Angle
+# Calculate azimuthal Angle ???????????? Delete????????????????
 def az_ang(up, top, north):
 	# up = vector pointing directly up
 	# top = vector pointing from bottom top of face of solar collectors (y)
@@ -300,6 +350,7 @@ def az_ang(up, top, north):
 		az = 360 - az
 	return az
 
+# Look up true az and el angles along with calculated ones ???????????? Delete????????????????
 def solar_angs(handle):
 	[mX, mY, mZ, aX, aY, aZ] = read_LJ_CA(handle)
 	
@@ -353,14 +404,9 @@ def read_LJ_LT(handle, AIN_D, AIN_NS, AIN_EW):
 	LT_NS = ljm.eReadName(handle, AIN_NS) # North/South Voltage reading
 	LT_EW = ljm.eReadName(handle, AIN_EW) # East/West votage reading
 	
-	if 1 == 1:
-		print('Direct: ', LT_direct)
-		print('NS: ', LT_NS)
-		print('EW: ', LT_EW)
-	
 	return LT_direct, LT_NS, LT_EW
 
-# Check that power meter is writing to file
+#  Check that power meter is writing to file: the power meter requires its own software called PowerMax PC and cannot be read directly with Python. Therefore, the output of the power meter data to a separate file then read it from there. The power meter must be outputting data before it can be read, so this function checks that the power meter is outputting data.
 def check_PM(PM_fpath):
 	# PM_fpath = file path for power meter data
 	
@@ -369,17 +415,20 @@ def check_PM(PM_fpath):
 	while lookPM:
 		
 		try:
-			# Look at file
+			# Read Power Meter file
+			# ???????????? Rewrite to check if numlines is growing??????????????
 			PMf = open(PM_fpath, 'r') # Open file
 			numlines = len(PMf.readlines()) # Get the number of lines
 			PMf.close()  # close file
 			
+			# Check to ensure file contains more than just the header
 			if numlines >3: 
 				print('Reading Power Meter data!')
 				print(numlines)
 				lookPM = False
-				
-			else: # If the file is not collecting data
+
+			# If the Power meter is not collecting data, this conditional gives the user time to start it	
+			else: 
 				print('\n')
 				while True:
 					PM_response = input('Power Meter file is very short and may not be collecting data!!\n Would you like to try again? (y/n)\n ')
@@ -399,7 +448,7 @@ def check_PM(PM_fpath):
 					else:
 						print('Invalid response. Please enter y for yes or n for no\n')
 		
-		# If there is an error reading the PM file
+		# This exception handles an error reading the PM file. I'm not sure if this will ever be used
 		except Exception as e:
 			print('Power Meter Error:', e)
 			while True:
@@ -407,60 +456,77 @@ def check_PM(PM_fpath):
 				if PM_response == 'y':
 					break
 				elif PM_response == 'n':
-					sys.exit()
 					print('Exiting program')
 					lookPM = False
+					sys.exit()
 				else:
 					print('Invalid response. Please enter y for yes or n for no\n')
 
-# Read Power Meter data from file
+# Read Power Meter data from its own output file
 def read_PM(PM_fpath):
-	PM_data = pd.read_csv(PM_fpath, header = 2, index_col = 'Timestamp') # read power file
+	PM_data = pd.read_csv(PM_fpath, header = 2, index_col = 'Timestamp') # Read power file
 	power = PM_data['0293A13R:Watts'][-1] # Retrieve latest power measurement
-	return power
+	return power # Return measurement
 
-# Build header for data file
-# This function depends on global variables
-def build_header(TCON, TC_num, PyrON, ComAccelON, LTON, PMON):
+# This function uses global toggles to create a header for all variables that are being used
+def build_header(CJCON, TCON, TC_num, PyrON, ComAccelON, LTON, PMON):
 	
 	header = 'Time'
+	
+	# Add TCs to header if using
 	if TCON == 1:
-		for i in range(0,TC_num):
-			header = header + sep + 'TC' + str(i+1)
+		for i in range(0, TC_num):
+			header = header + sep + 'Temp' + str(i+1)
+		for i in range(0, TC_num):
+			header = header + sep + 'TCV' + str(i+1)
+			
+	# Add Cold Junction Sensor to header if using
+	if CJCON == 1:
+		header = header + sep + 'CJCT' + sep + 'CJCV'
 
+	# Add pyrheliometer to header if using
 	if PyrON == 1:
 		header = header + ',PyrO,PyrE'
 
+	# Add Compass and Accelerometer variables to header if using
 	if ComAccelON == 1:
-		# header = header + ',Mag X,Mag Y,Mag Z,Accel X,Accel Y,Accel Z,Elevation,El True,Azimuth,Az True'
 		header = header + ',Mag X,Mag Y,Mag Z,Accel X,Accel Y,Accel Z,Elevation,Azimuth,El Ave,Az Ave,El True,Az True'
 
+	# Add Light Tower readings to header if using
 	if LTON == 1:
 		header = header + ',LT direct,LT NS,LT EW'
 
+	# Add power meter to the header if using
 	if PMON == 1:
 		header = header + ',Power'
 
-	header = header
+	header = header # Not sure if this is necessary ????????????????
+
 	return header
 
 # This function reads all requested instruments outputs their values to an output string
 # It depends on global variables
-def collect_data(handle, TCON, TC_num, TC_AIN_start, PyrON, Pyr_AIN, ComAccelON, LTON, AIN_D, AIN_NS, AIN_EW, PMON, PM_fpath):
-	# Initiate output string
+def collect_data(handle, CJCON, CJCAIN, TCON, TC_num, TC_AIN_start, TC_AIN_vec, PyrON, Pyr_AIN, ComAccelON, LTON, AIN_D, AIN_NS, AIN_EW, PMON, PM_fpath):
+	# Initiate output string with the first column as a timestamp
 	outstr =  datetime.now().strftime('%H:%M:%S.%f')
 	
-	# Read TCs
+	# Read TCs and include in outstr
 	if TCON == 1:
-		Temps = read_LJ_TC(handle, TC_num, TC_AIN_start)
-		outstr = outstr + sep + sep.join(map(str, Temps))
+		[Temps, TCVs] = read_LJ_TC(handle, TC_num, TC_AIN_start, TC_AIN_vec, CJCON, CJCAIN)
+		outstr = outstr + sep + sep.join(map(str, Temps)) # Temperatures
+		outstr = outstr + sep + sep.join(map(str, TCVs)) # Voltage readings
 	
-	# Read Pyrheliometer
+	# Read Cread Cold Junction Sensor and output to outstr
+	if CJCON == 1:
+		CJCT, CJCV = read_LJ_CJC(handle, TC_AIN_start) 
+		outstr = outstr + sep + str(CJCT) + sep + str(CJCV)
+	
+	# Read Pyrheliometer and output to outstr
 	if PyrON == 1:
 		[PO, PE] = read_LJ_Pyr(handle, Pyr_AIN)
 		outstr = outstr + sep + str(PO) + sep + str(PE)
 	
-	# Read Compass and Accelerometer
+	# Read Compass and Accelerometer and output to outstr
 	if ComAccelON == 1:
 		[mX, mY, mZ, aX, aY, aZ] = read_LJ_CA(handle)
 		[el, azim, aveel, aveaz, az_True, el_True] = solar_angs(handle)
@@ -470,12 +536,12 @@ def collect_data(handle, TCON, TC_num, TC_AIN_start, PyrON, Pyr_AIN, ComAccelON,
 		outstr = outstr + sep + str(el) + sep + str(azim) + sep + str(aveel) + sep + str(aveaz) + sep + str(el_True)  + sep + str(az_True)
 		# outstr = outstr + sep + str(el) + sep + str(el_True) + sep + str(azim) + sep + str(az_True)
 		
-	# Read Light Tower
+	# Read Light Tower and output to outstr
 	if LTON == 1:
 		[LT_direct, LT_NS, LT_EW] = read_LJ_LT(handle, AIN_D, AIN_NS, AIN_EW)
 		outstr = outstr + sep + str(LT_direct) + sep + str(LT_NS) + sep + str(LT_EW)
 	
-	# Read Power Meter
+	# Read Power Meter and output to outstr
 	if PMON == 1:
 		Power = read_PM(PM_fpath)
 		outstr = outstr + sep + str(Power)
@@ -486,21 +552,22 @@ def collect_data(handle, TCON, TC_num, TC_AIN_start, PyrON, Pyr_AIN, ComAccelON,
 ########################################
 # Tracker function
 
-# def Track(handle):
 def Track(handle, AIN_NS, LT_moveN, LT_stopN, FION, LT_moveS, LT_stopS, FIOS, AIN_EW, LT_moveW, LT_stopW, FIOW, LT_moveE, LT_stopE, FIOE):
-	# Move North
+	
+	# Try moving
 	try:
+		# Move North
 		if ljm.eReadName(handle, AIN_NS) < LT_moveN:
 			while ljm.eReadName(handle, AIN_NS) < LT_stopN:
 				ljm.eWriteName(handle, FION,1)
-				print('Move North. LT NS: ', ljm.eReadName(handle, AIN_NS))
+				print('Moving North. LT NS: ', ljm.eReadName(handle, AIN_NS))
 			ljm.eWriteName(handle, FION, 0)
 		
 		# Move South
 		if ljm.eReadName(handle, AIN_NS) > LT_moveS:
 			while ljm.eReadName(handle, AIN_NS) > LT_stopS:
 				ljm.eWriteName(handle, FIOS,1)
-				print('Move South. LT NS: ', ljm.eReadName(handle, AIN_NS))
+				print('Moving South. LT NS: ', ljm.eReadName(handle, AIN_NS))
 			ljm.eWriteName(handle, FIOS, 0)
 		
 		# Move West
@@ -516,6 +583,8 @@ def Track(handle, AIN_NS, LT_moveN, LT_stopN, FION, LT_moveS, LT_stopS, FIOS, AI
 				print('Moving East. LT EW: ', ljm.eReadName(handle, AIN_EW))
 				ljm.eWriteName(handle, FIOE, 1)
 			ljm.eWriteName(handle, FIOE, 0)
+	
+	# If there's an issue, stop moving
 	except:
 		ljm.eWriteName(handle, FION, 0)
 		ljm.eWriteName(handle, FIOS, 0)
@@ -529,3 +598,5 @@ def Track(handle, AIN_NS, LT_moveN, LT_stopN, FION, LT_moveS, LT_stopS, FIOS, AI
 				exit()
 			else:
 				in1 = input('Invalid input. Please enter y or n. \n')
+
+################# End of Library ###################
